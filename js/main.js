47707041ddc90aba9d4a105ff429bad2d139931e
keiYamas <90538073+keiYamas@users.noreply.github.com>
2022-02-13 21:12:55 +0900

    add page_top

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function () {
   $('.main_visual__image').fadeIn(2600);
+
+  $( '.page_top' ).click(
+    function()
+    {
+      $( 'html,body' ).animate( {scrollTop:0} , 'slow' ) ;
+    }
+  ) ;
 });
